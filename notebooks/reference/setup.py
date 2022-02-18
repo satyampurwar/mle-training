c4fe130dfd40da72fd5951cf49b833749fe8c3cd
satyam-ta <satyam.purwar@tigeranalytics.com>
2022-02-18 13:32:26 +0530

    reference for dynamic configuration

---
+++ notebooks/reference/setup.py
@@ -0,0 +1,24 @@
+# import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+# setuptools.setup(
+#     name="housing_value",
+#     version="0.0.0",
+#     author="satyam-ta",
+#     author_email="satyam.purwar@tigeranalytics.com",
+#     description="housing value package",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/satyam-ta/mle-training",
+#     project_urls={"Bug Tracker": "https://github.com/satyam-ta/mle-training/issues"},
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     package_dir={"": "src"},
+#     packages=setuptools.find_packages(where="src"),
+#     python_requires=">=3.6",
+# )
