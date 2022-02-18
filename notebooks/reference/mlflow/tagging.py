b28dffd22a994dad0a5ce6483834c3ac6c2314d5
satyam-ta <satyam.purwar@tigeranalytics.com>
2022-02-18 13:46:53 +0530

    deleted or location changed

--- notebooks/reference/mlflow/tagging.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-from mlflow.tracking import MlflowClient
-
-client = MlflowClient()
-_run = client.get_run(run_id="993ac1452899493792a4440948c579c3")
-dt = datetime.now().strftime("%d-%m-%Y (%H:%M:%S.%f)")
-client.set_tag(_run.info.run_id, "deployed", dt)
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-if __name__ == "__main__":
-    setup(
-        name="housing_value",
-        packages=find_packages(where="src"),
-        package_dir={"": "src"},
-    )
